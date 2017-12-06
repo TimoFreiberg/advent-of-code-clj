2266735b94c7d4303ee7c964c0eff830f500bedf
Timo Freiberg <timo.freiberg@gmail.com>
2017-12-06 21:51:44 +0100

    Create arg-flipped string split fn in main

--- src/advent_of_code/main.clj
+++ src/advent_of_code/main.clj
@@ -3,5 +3,7 @@
 (defn find-first [pred coll]
   (first (filter pred coll)))
 
+(defn split [regex string] (clojure.string/split string regex))
+
 (defn load-input-file "load-input-file [day-number] loads content of the input file for the given day " [day-number]
   (slurp (str "resources/day" day-number)))
