2db83488713b4c8bd489a9b9e4d5fe3a7cac4d33
Timo Freiberg <timo.freiberg@gmail.com>
2017-12-08 14:06:21 +0100

    day7 increment (CIDER middleware exception)

--- src/advent_of_code/day7.clj
+++ src/advent_of_code/day7.clj
@@ -1,19 +1,23 @@
 (ns advent-of-code.day7 (:require [advent-of-code.main :refer :all]))
 
+(defn split-commas [s] (clojure.string/split s #","))
+
 (defn parse-line [line]
   (let [[base children] (clojure.string/split line #"->")
-        [name weight] (split-words base)]
-    (run! println [base children name weight])))
+        [name weight] (split-words base)
+        children-vec (->> children
+                          (fnil split-commas "")
+                          (map clojure.string/trim))]
+    [name children-vec]))
 
 (defn solve-1 [input]
   (->> input
        (clojure.string/split-lines)
-       (mapv parse-line)
+       (take 10)
+       (trace)
+       (mapv parse-line)))
 
-       (run! println)))
-
-(defn solve-2 [input]
-  )
+(defn solve-2 [input])
 
 (defn solve-day-7 []
   (let [input (load-input-file "7")]
