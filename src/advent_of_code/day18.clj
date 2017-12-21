1602d5a7487b58aecfa5e9a8903f0ccba7152729
Timo Freiberg <timo.freiberg@gmail.com>
2017-12-21 22:32:35 +0100

    Start day18

---
+++ src/advent_of_code/day18.clj
@@ -0,0 +1,11 @@
+(ns advent-of-code.day18 (:require [advent-of-code.main :refer :all]))
+
+(defn solve-1 [steps])
+
+(defn solve-2 [steps])
+
+(def input (load-input-file "18"))
+
+(defn solve-day-18 []
+  (clojure.pprint/pprint (solve-1 input))
+  (clojure.pprint/pprint (solve-2 input)))
