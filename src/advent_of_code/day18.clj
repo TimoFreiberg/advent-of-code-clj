c50e886673eab4df171ad6f499b14f7f1afe8de8
Timo Freiberg <timo.freiberg@gmail.com>
2017-12-27 23:47:37 +0100

    Start reworking day18 in a simpler way

--- src/advent_of_code/day18.clj
+++ src/advent_of_code/day18.clj
@@ -25,9 +25,6 @@
     (trace "storing" new-val "in" x)
     (set-val state x (op x-val y-val))))
 
-(defn recover-val [state]
-  (get state :snd))
-
 (defn cmd-sound [state x]
   (let [x-val (get-val state x)]
     (trace "sound:" x "/" x-val)
@@ -66,13 +63,6 @@
   (inc-cursor
    (apply-fn-to-reg state x y mod)))
 
-(defn cmd-recover [state x]
-  (let [recovered (recover-val state)]
-    (if (not= 0 (get-val state x))
-      (trace "recover" x "val" recover-val)
-      (trace "don't recover" x "because its val is 0"))
-    (inc-cursor state)))
-
 (defn cmd-jgz [state x y]
   (let [y-val (get-val state y)]
     (if (> (get-val state x) 0)
@@ -92,44 +82,48 @@
   {:instruction-count (count instructions)
    :cursor 0})
 
-(defn apply-instruction [instruction state]
-  (let [{op :op arg-1 :arg-1 arg-2 :arg-2} instruction]
-    (if (contains? instruction :arg-2)
+(defn apply-instruction-common [instruction state]
+  (let [{:keys [op arg-1 arg-2]} instruction
+        cursor (:cursor state)
+        state (inc-cursor state)
+        get-val (partial get-val state)]
+    (case op
+      :set (set-val state arg-1 (get-val arg-2))
+      :add (set-val state arg-1 (+
+                                 (get-val arg-1)
+                                 (get-val arg-2)))
+      :mul (set-val state arg-1 (*
+                                 (get-val arg-1)
+                                 (get-val arg-2)))
+      :mod (set-val state arg-1 (mod
+                                 (get-val arg-1)
+                                 (get-val arg-2)))
+      :jgz (if (pos? (get-val arg-1))
+             (assoc state :cursor (+
+                                   cursor
+                                   (get-val arg-2)))
+             state)
       (do
-        (op state arg-1 arg-2))
-      (do
-        (op state arg-1)))))
+        (println "could not apply instruction" op)
+        state))))
+
+(defn apply-instruction-1 [instruction state]
+  (let [{:keys [op arg-1 arg-2]} instruction
+        cursor (:cursor state)
+        _ (if (nil? (:cursor state)) (prn state))
+        state (update state :cursor inc)
+        get-val (partial get-val state)]
+    (case op
+      :snd (assoc state :snd (get-val arg-1))
+      :rcv (if (pos? (get-val arg-1))
+             (assoc state :recovered (:snd state))
+             state)
+      (apply-instruction-common instruction state))))
 
 (defn both-blocked? [[log-entry-1 log-entry-2]]
   (and
    (get-in log-entry-1 [1 :blocked])
    (get-in log-entry-2 [1 :blocked])))
-
-(defn run-program-on-state [init-state instructions]
-  (take-while
-   #(cursor-in-range (second %))
-   (iterate
-    (fn ap-instr [[_ state]]
-      (let [instruction (get instructions (get state :cursor))]
-        [instruction
-         (apply-instruction instruction state)]))
-    [nil init-state])))
-
-(defn run-program [instructions]
-  (run-program-on-state (mk-init-state instructions) instructions))
-
-(defn run-2-programs [instructions]
-  (let [state-1 (assoc-in (mk-init-state instructions) [:reg :p] 0)
-        state-2 (assoc-in state-1 [:reg :p] 1)
-        queue-1-to-2 (java.util.ArrayDeque.)
-        queue-2-to-1 (java.util.ArrayDeque.)
-        state-1 (assoc state-1 :queue-out queue-1-to-2 :queue-in queue-2-to-1)
-        state-2 (assoc state-2 :queue-out queue-2-to-1 :queue-in queue-1-to-2)
-        prog-1-log (run-program-on-state state-1 instructions)
-        prog-2-log (run-program-on-state state-2 instructions)]
-    (take-while
-     (comp not both-blocked?)
-     (map vector prog-1-log prog-2-log))))
 
 (defn keyword-or-number [s]
   (if (nil? s)
@@ -138,71 +132,29 @@
       (Integer/parseInt s)
       (keyword s))))
 
-(defn parse-instruction-1 [line]
-  (let [[cmd arg-1 arg-2] (split-words line)
-        arg-1 (keyword-or-number arg-1)
-        arg-2 (keyword-or-number arg-2)]
-    (case cmd
-      "snd" {:op cmd-sound :arg-1 arg-1}
-      "set" {:op cmd-set :arg-1 arg-1 :arg-2 arg-2}
-      "add" {:op cmd-add :arg-1 arg-1 :arg-2 arg-2}
-      "mul" {:op cmd-mul :arg-1 arg-1 :arg-2 arg-2}
-      "mod" {:op cmd-mod :arg-1 arg-1 :arg-2 arg-2}
-      "rcv" {:op cmd-recover :arg-1 arg-1}
-      "jgz" {:op cmd-jgz :arg-1 arg-1 :arg-2 arg-2})))
+(defn parse-instruction [line]
+  (let [[cmd arg-1 arg-2] (mapv keyword-or-number (split-words line))]
+    {:op cmd :arg-1 arg-1 :arg-2 arg-2}))
 
-(defn parse-instruction-2 [line]
-  (update
-   (parse-instruction-1 line)
-   :op
-   (fn [op]
-     (condp = op
-       cmd-sound cmd-send
-       cmd-recover cmd-receive
-       op))))
-
-(defn parse-input-1 [s]
+(defn parse-input [s]
   (->> s
        (clojure.string/split-lines)
-       (mapv parse-instruction-1)))
-
-(defn parse-input-2 [s]
-  (->> s
-       (clojure.string/split-lines)
-       (mapv parse-instruction-2)))
-
-(defn op-is [op log-entry]
-  (= op (get-in log-entry [0 :op])))
-
-(defn arg-val-of-op-is [n log-entry]
-  (let [[{arg :arg-1} state] log-entry
-        arg-val (get-val state arg)]
-    (= n arg-val)))
-
+       (mapv parse-instruction)))
 
 (defn solve-1 [steps]
-  (let [prog-log (->> steps
-                      (parse-input-1)
-                      (run-program))]
-    (->> prog-log
-         (filter #(op-is cmd-recover %))
-         (filter #(not (arg-val-of-op-is 0 %)))
-         (first))))
+  (let [instructions (parse-input steps)]
+    (loop [state (mk-init-state instructions)]
+      (let [recovered (:recovered state)
+            cursor (:cursor state)]
+        (if recovered
+          recovered
+          (recur (apply-instruction-1
+                  (get instructions (:cursor state))
+                  state)))))))
 
-(defn solve-2 [steps]
-  (let [prog-logs (->> steps
-                      (parse-input-2)
-                      (run-2-programs))]
-    (->> prog-logs
-         (filter #(op-is cmd-send (first %)))
-         ;; (take 15)
-         (trace-pprint)
-         (count)))
-  )
+(defn solve-2 [steps])
 
 (def input (load-input-file "18"))
-(def parsed-input-1 (parse-input-1 input))
-(def parsed-input-2 (parse-input-2 input))
 
 (defn solve-day-18 []
   (clojure.pprint/pprint (solve-1 input))
