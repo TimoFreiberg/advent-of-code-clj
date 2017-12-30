46b73b85a717b7f0bc9e8048e0c964f4ed395047
Timo Freiberg <timo.freiberg@gmail.com>
2017-12-30 17:18:07 +0100

    Add input parsing to day20

--- src/advent_of_code/day20.clj
+++ src/advent_of_code/day20.clj
@@ -1,6 +1,40 @@
 (ns advent-of-code.day20 (:require
-                          [advent-of-code.main :refer :all]))
+                          [advent-of-code.main :refer :all]
+                          [instaparse.core :as insta]))
 
+(def line-parser
+  (insta/parser
+   "<line> = pos comma vel comma accel
+<comma> = <',' #'\\s*'>
+pos = <'p=<'> num comma num comma num <'>'>
+vel = <'v=<'> num comma num comma num <'>'>
+accel = <'a=<'> num comma num comma num <'>'>
+<num> = #'(-)?\\d+'"
+   :output-format :enlive))
+
+(defn parse-line [line]
+  (let [parsed (line-parser line)
+        mk-vec (fn [xs] (mapv #(Integer/parseInt %) xs))
+        [pos vel accel] (mapv (comp mk-vec :content) parsed)]
+    {:pos pos :vel vel :accel accel}))
+
+(defn parse-input [input]
+  (->> input
+      (clojure.string/split-lines)
+      (mapv parse-line)))
+
+(defn +vec [vec1 vec2]
+  (mapv + vec1 vec2))
+
+(defn tick [{:keys [pos vel accel] :as particle}]
+  (-> particle
+      (update :pos +vec vel)
+      (update :vel +vec accel)))
+
+(defn manhattan-distance [{:keys [pos]}]
+  (->> pos
+       (mapv #(Math/abs %))
+       (reduce + 0)))
 
 (defn solve-1 [input]
   )
