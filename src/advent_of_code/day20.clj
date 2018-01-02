e5ba8dad174afba26652d96e41a65b5519e5617f
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2018-01-02 17:12:47 +0100

    Inc day20

--- src/advent_of_code/day20.clj
+++ src/advent_of_code/day20.clj
@@ -20,8 +20,9 @@ accel = <'a=<'> num comma num comma num <'>'>
 
 (defn parse-input [input]
   (->> input
-      (clojure.string/split-lines)
-      (mapv parse-line)))
+       (clojure.string/split-lines)
+       (mapv parse-line)
+       (mapv #(assoc %2 :ix %1) (range))))
 
 (defn +vec [vec1 vec2]
   (mapv + vec1 vec2))
@@ -31,16 +32,91 @@ accel = <'a=<'> num comma num comma num <'>'>
       (update :pos +vec vel)
       (update :vel +vec accel)))
 
-(defn manhattan-distance [{:keys [pos]}]
-  (->> pos
+(defn manhattan-distance [vec]
+  (->> vec
        (mapv #(Math/abs %))
        (reduce + 0)))
 
+(defn by-accel-then-vel [a b]
+  (let [compare-by-keys (fn [particle]
+                          (mapv
+                           #(manhattan-distance (% particle))
+                           [:accel :vel :pos]))]
+    (compare (compare-by-keys a) (compare-by-keys b))))
+
+(defn retain-x-with-zero-y [p x-key y-key]
+  (update
+   p x-key
+   (fn [x]
+     (->> x
+          (map vector (y-key p))
+          (filter #(= 0 (first %)))
+          (map second)))))
+
+(defn find-particle-with-lowest-acceleration [particles]
+  (let [accel-total (comp manhattan-distance :accel)
+        min-accel (apply min (map accel-total particles))
+        min-accel-particles (filter #(= min-accel (accel-total %)) particles)
+        static-vel-particles (map
+                              #(retain-x-with-zero-y % :vel :accel)
+                              min-accel-particles)
+        static-pos-particles (map
+                              #(retain-x-with-zero-y % :pos :vel)
+                              static-vel-particles)]
+    (->> static-pos-particles
+         (sort by-accel-then-vel)
+         (first)
+         (:ix))))
+
 (defn solve-1 [input]
-  )
+  (->> input
+       (parse-input)
+       (find-particle-with-lowest-acceleration)))
+
+(defn find-duplicate-positions [particles]
+  (loop [stack particles
+         found-positions #{}
+         duplicate-positions #{}]
+    (if (empty? stack)
+      duplicate-positions
+      (let [current-pos (:pos (first stack))]
+        (if (contains? found-positions current-pos)
+          (recur
+           (rest stack)
+           found-positions
+           (conj duplicate-positions current-pos))
+          (recur
+           (rest stack)
+           (conj found-positions current-pos)
+           duplicate-positions))))))
+
+(defn remove-colliding-particles [particles]
+  (let [duplicate-positions (into #{}
+                                  (for [[id freq] (frequencies (mapv :pos particles))
+                                        :when (> freq 1)]
+                                    id))]
+    (filter #(not (contains? duplicate-positions (:pos %))) particles)))
+
+(defn run-simulation-tick [state]
+  (-> state
+      (update :particles
+              #(->> %
+                    (mapv tick)
+                    (remove-colliding-particles)))
+      (update :iterations inc)))
+
+(defn resolve-collisions [particles]
+  (loop [state particles
+         counter 0
+         ])
+  (take 5000 (iterate run-simulation-tick {:iterations 0 :particles particles})))
 
 (defn solve-2 [input]
-  )
+  (->> input
+       (parse-input)
+       (resolve-collisions)
+       (last)
+       (count)))
 
 (def input (load-input-file "20"))
 
