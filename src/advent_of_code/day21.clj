48ca4b6df6c34f0e0b635bf6c926c0460187d2d4
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2018-01-03 16:15:36 +0100

    Start day21

---
+++ src/advent_of_code/day21.clj
@@ -0,0 +1,14 @@
+(ns advent-of-code.day21 (:require
+                          [advent-of-code.main :refer :all]))
+
+(defn solve-1 [input]
+  )
+
+(defn solve-2 [input]
+  )
+
+(def input (load-input-file "21"))
+
+(defn solve-day-21 []
+  (clojure.pprint/pprint (solve-1 input))
+  (clojure.pprint/pprint (solve-2 input)))
